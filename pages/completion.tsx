1fbe9361ba6bac6e49565f970c8c090210508b73
Anna Milena Papanakli <annamilenapapanakli@air-von-anna.home>
2025-06-06 15:37:44 +0200

    added survey swap karma code

--- pages/completion.tsx
+++ pages/completion.tsx
@@ -18,7 +18,7 @@ export default function Complete() {
     <div className="min-h-screen flex flex-col justify-center items-center p-4 text-center">
       <h1 className="text-2xl font-bold mb-4">Thank you for participating!</h1>
       <p className="text-gray-700">Your input is valuable to our research.</p>
-      <p className="mt-2 text-sm text-gray-500">If you came from SurveySwap here is your Karma: V3GR-DYBX-OL7U .</p>
+      <p className="mt-2 text-sm text-gray-500">The following code gives you Karma that can be used to get free research participants at SurveySwap.io. Go to: https://surveyswap.io/sr/V3GR-DYBX-OL7U Or, alternatively, enter the code manually: V3GR-DYBX-OL7U</p>
     </div>
   );
 }

71ed3ce23e303fcb9e1fd7629eec77d2d30733e3
Anna Milena Papanakli <annamilenapapanakli@air-von-anna.home>
2025-06-06 09:34:19 +0200

    fixed some minor errors

--- pages/postsurvey.tsx
+++ pages/postsurvey.tsx
@@ -46,7 +46,7 @@ export default function PostSurvey() {
 
   return (
     <div className="p-4 max-w-md mx-auto">
-     <h1 className="text-xl font-bold mb-4">✨ Yay you did it! Some last questions</h1>
+     <h1 className="text-xl font-bold mb-4">✨ Yay you did it! These are the last questions!</h1>
 
     
       <label className="block mt-4 p-2 mb-1 font-semibold">Which method was the most helpful?</label>
@@ -94,7 +94,7 @@ export default function PostSurvey() {
 
       <div className="flex justify-end">
         <button onClick={handleSubmit} className="px-4 py-2 bg-green-600 text-white rounded">
-          Submit
+          You answered everything? Then submit!
         </button>
       </div>
 
